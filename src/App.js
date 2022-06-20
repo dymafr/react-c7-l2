8b7df1c5dd05d4684a10fad6051617cc6628ff5d
dymafr <erwan1309@hotmail.com>
2022-06-20 11:24:09 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function handleClick(e) {
+  e.stopPropagation();
   console.log('CLIC', e);
 }
 
@@ -8,13 +9,24 @@ function handleInput(e) {
   console.log('INPUT', e);
 }
 
+function handleFocus(e) {
+  console.log('FOCUS', e);
+}
+
+function handleClickDiv() {
+  console.log('CLIC DIV');
+}
+
 function App() {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center p-20">
+    <div
+      onClick={handleClickDiv}
+      className="d-flex flex-column justify-content-center align-items-center p-20"
+    >
       <button onClick={handleClick} className="mb-20">
         Submit
       </button>
-      <input type="text" onInput={handleInput}></input>
+      <input type="text" onInput={handleInput} onInput={handleFocus} />
     </div>
   );
 }
